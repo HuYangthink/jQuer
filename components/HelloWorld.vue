5ccf67082283bc4f88686bcba9bc3aeda53e30d6
HuYangthink <41947204+HuYangthink@users.noreply.github.com>
2018-11-15 16:21:08 +0800

    Add files via upload

---
+++ components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+<div id="waa">{{myApp}}</div>
+</template>
+
+<script>
+export default{
+  name :'HelloWorld',
+   data(){
+   return{
+   msg:'Welcome to your Vue.js App'
+   
+   }
+   }
+}
+
+</script>
+
+</style>
