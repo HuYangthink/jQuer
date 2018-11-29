026ead69c91ebeb6088f006e851d8caf8f0df9d9
HuYangthink <41947204+HuYangthink@users.noreply.github.com>
2018-11-29 17:31:57 +0800

    Add files via upload

---
+++ index/2.js
@@ -0,0 +1,246 @@
+$(function(){
+  $(".ynzc_id a").mouseover(function(){
+
+    $('.ynzc_id a').css("color",'#aa2116');
+  }).mouseout(function(){
+    $('.ynzc_id a').css("color",'#808080');
+  });
+
+
+  // 登陆注册
+  $(".ynzc_dlzc a").mouseover(function(){
+    $('.ynzc_dlzc a').css('color',"#aa2116");
+  }).mouseout(function(){
+    $('.ynzc_dlzc a').css('color','#808080');
+  });
+
+// 新人福利  会员
+$('.xrfl').mouseover(function(){
+  $(".xrfl").css('color','#fff'),
+  $('.xrfl').css('background','red');
+}).mouseout(function(){
+  $('.xrfl').css('color','red'),
+  $('.xrfl').css('background',"#fffef9");
+});
+
+
+$(".hy").mouseover(function(){
+  $(".hy").css('color','#fff'),
+  $('.hy').css('background','#c63c26');
+}).mouseout(function(){
+  $('.hy').css('color','#fdb933'),
+  $('.hy').css('background','#130c0e');
+});
+
+// 促销   公告    更多
+
+$("#cx a").mouseover(function(){
+  $('.ycts1').css("display","block"),
+  $('.ycts2').css("display",'none'),
+  $('#cx a').css('color','red');
+}).mouseout(function(){
+  $('#cx a').css('color','#808080')
+})
+
+$('#gg a').mouseover(function(){
+  $('.ycts1').css('display','none'),
+  $('.ycts2').css("display",'block'),
+  $('#gg a').css('color','red');
+}).mouseout(function(){
+  $('#gg a').css("color",'#808080');
+});
+
+
+// 隐藏的字体
+
+$('.ycts_a a').mouseover(function(){
+  $(".ycts_a a").css('color','red');
+}).mouseout(function(){
+  $('.ycts_a a').css('color','#808080');
+});
+
+$('.ycts_b a').mouseover(function(){
+  $(".ycts_b a").css('color','red');
+}).mouseout(function(){
+  $('.ycts_b a').css('color','#808080');
+});
+
+$('.ycts_c a').mouseover(function(){
+  $(".ycts_c a").css('color','red');
+}).mouseout(function(){
+  $('.ycts_c a').css('color','#808080');
+});
+
+$('.ycts_d a').mouseover(function(){
+  $(".ycts_d a").css('color','red');
+}).mouseout(function(){
+  $('.ycts_d a').css('color','#808080');
+});
+
+$('.ycts_e a').mouseover(function(){
+  $(".ycts_e a").css('color','red');
+}).mouseout(function(){
+  $('.ycts_e a').css('color','#808080');
+});
+
+$('.ycts_f a').mouseover(function(){
+  $(".ycts_f a").css('color','red');
+}).mouseout(function(){
+  $('.ycts_f a').css('color','#808080');
+});
+
+$('.ycts_g a').mouseover(function(){
+  $(".ycts_g a").css('color','red');
+}).mouseout(function(){
+  $('.ycts_g a').css('color','#808080');
+});
+
+$('.ycts_h a').mouseover(function(){
+  $(".ycts_h a").css('color','red');
+}).mouseout(function(){
+  $('.ycts_h a').css('color','#808080');
+});
+
+
+
+// 最下面
+$(".dyp_a").mouseover(function(){
+  $(".dyp_a_2 a").css("color",'red'),
+    $(".dyp_a_1 img").attr('src','../../lx/JD/img/hf1.png')
+}).mouseout(function(){
+  $(".dyp_a_2 a").css('color','#808080'),
+  $('.dyp_a_1 img').attr('src','../../lx/jd/img/hf.png')
+})
+
+$(".dyp_b").mouseover(function(){
+  $(".dyp_b_2 a").css("color",'red'),
+    $(".dyp_b_1 img").attr('src','../../lx/JD/img/jp1.png')
+}).mouseout(function(){
+  $(".dyp_b_2 a").css('color','#808080'),
+  $('.dyp_b_1 img').attr('src','../../lx/jd/img/jp.png')
+})
+
+
+$(".dyp_c").mouseover(function(){
+  $(".dyp_c_2 a").css("color",'red'),
+    $(".dyp_c_1 img").attr('src','../../lx/JD/img/jiudian1.png')
+}).mouseout(function(){
+  $(".dyp_c_2 a").css('color','#808080'),
+  $('.dyp_c_1 img').attr('src','../../lx/jd/img/jiudian.png')
+})
+
+$(".dyp_d").mouseover(function(){
+  $(".dyp_d_2 a").css("color",'red'),
+    $(".dyp_d_1 img").attr('src','../../lx/JD/img/yx1.png')
+}).mouseout(function(){
+  $(".dyp_d_2 a").css('color','#808080'),
+  $('.dyp_d_1 img').attr('src','../../lx/jd/img/yx.png')
+})
+
+$(".drp_a").mouseover(function(){
+  $(".drp_a_2 a").css("color",'red'),
+    $(".drp_a_1 img").attr('src','../../lx/JD/img/qy1.png')
+}).mouseout(function(){
+  $(".drp_a_2 a").css('color','#808080'),
+  $('.drp_a_1 img').attr('src','../../lx/jd/img/qy.png')
+})
+
+
+$(".drp_b").mouseover(function(){
+  $(".drp_b_2 a").css("color",'red'),
+    $(".drp_b_1 img").attr('src','../../lx/JD/img/jyk1.png')
+}).mouseout(function(){
+  $(".drp_b_2 a").css('color','#808080'),
+  $('.drp_b_1 img').attr('src','../../lx/jd/img/jyk.png')
+})
+
+
+$(".drp_c").mouseover(function(){
+  $(".drp_c_2 a").css("color",'red'),
+    $(".drp_c_1 img").attr('src','../../lx/JD/img/dyp1.png')
+}).mouseout(function(){
+  $(".drp_c_2 a").css('color','#808080'),
+  $('.drp_c_1 img').attr('src','../../lx/jd/img/dyp.png')
+})
+
+
+$(".drp_d").mouseover(function(){
+  $(".drp_d_2 a").css("color",'red'),
+    $(".drp_d_1 img").attr('src','../../lx/JD/img/hcp1.png')
+}).mouseout(function(){
+  $(".drp_d_2 a").css('color','#808080'),
+  $('.drp_d_1 img').attr('src','../../lx/JD/img/hcp.png')
+});
+
+
+$(".dsp_a").mouseover(function(){
+  $(".dsp_a_2 a").css("color",'red'),
+    $(".dsp_a_1 img").attr('src','../../lx/JD/img/zc1.png')
+}).mouseout(function(){
+  $(".dsp_a_2 a").css('color','#808080'),
+  $('.dsp_a_1 img').attr('src','../../lx/JD/img/jdzc.png')
+})
+
+
+$(".dsp_b").mouseover(function(){
+  $(".dsp_b_2 a").css("color",'red'),
+    $(".dsp_b_1 img").attr('src','../../lx/JD/img/lc1.png')
+}).mouseout(function(){
+  $(".dsp_b_2 a").css('color','#808080'),
+  $('.dsp_b_1 img').attr('src','../../lx/JD/img/lc.png')
+})
+
+$(".dsp_c").mouseover(function(){
+  $(".dsp_c_2 a").css("color",'red'),
+    $(".dsp_c_1 img").attr('src','../../lx/JD/img/lp1.png')
+}).mouseout(function(){
+  $(".dsp_c_2 a").css('color','#808080'),
+  $('.dsp_c_1 img').attr('src','../../lx/JD/img/lp.png')
+})
+
+
+$(".dhp").mouseover(function(){
+  $(".dhp_a_2 a").css("color",'red'),
+    $(".dhp_a_1 img").attr('src','../../lx/JD/img/bt1.png')
+}).mouseout(function(){
+  $(".dhp_a_2 a").css('color','#808080'),
+  $('.dhp_a_1 img').attr('src','../../lx/JD/img/bt.png')
+})
+
+
+// 底部
+$('.hfcz a').mouseover(function(){
+  $(".hfcz a").css("color",'red'),
+  $(".xmyc").css("display",'block'),
+  $("#llcz").css("display",'none'),
+  $("#tcbg").css('display','none');
+}).mouseout(function(){
+  $('.hfcz a').css('color','#808080');
+  // $(".xmyc").css("display",'none');
+})
+
+
+//
+
+
+
+$('.llcz a').mouseover(function(){
+  $(".llcz a").css('color','red'),
+  $(".xmyc").css("display",'none'),
+  $("#llcz").css("display",'block'),
+  $("#tcbg").css('display','none');
+}).mouseout(function(){
+  $(".llcz a").css("color",'#808080');
+
+})
+
+$(".tcbg a").mouseover(function(){
+  $(".tcbg a").css("color",'red'),
+  $(".xmyc").css("display",'none'),
+  $("#llcz").css("display",'none'),
+  $("#tcbg").css('display','block');
+}).mouseout(function(){
+  $(".tcbg a").css('color','#808080');
+})
+
+})
