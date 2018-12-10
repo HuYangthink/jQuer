73afd265026696f117c1b8157cf57c0c303f0d7f
HuYangthink <41947204+HuYangthink@users.noreply.github.com>
2018-12-10 17:12:22 +0800

    Add files via upload

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+
+<!-- <div id="first">
+{{myfirst}}
+</div> -->
+</template>
+
+<script>
+
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/first.vue
@@ -0,0 +1,10 @@
+<template >
+
+
+  <input type="text" id="txt" name="" value="">
+</template>
+<script type="text/javascript" src="https://unpkg.com/vue"></script>
+
+<script type="text/javascript">
+
+</script>
