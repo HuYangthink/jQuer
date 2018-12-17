4a8adcae044b60b2ee0a939c55e6572785d53b20
HuYangthink <41947204+HuYangthink@users.noreply.github.com>
2018-12-17 17:06:37 +0800

    Add files via upload

---
+++ zy/jl.js
@@ -0,0 +1,71 @@
+$(function(){
+  setTimeout(function(){
+    $('.zcwz').show(2000);
+  },5000),
+  setTimeout(function(){
+    $('.zcwz').hide(2000);
+  },10000);
+  setTimeout(function(){
+    $('.wz').show(3000);
+  },15000),
+  setTimeout(function(){
+    $('#first img').show(3000);
+  },20000);
+
+  setTimeout(function(){
+    $('#first').hide(2000);
+  },25000);
+
+
+  setTimeout(function(){
+    $('.wz2').show(3000);
+  },30000),
+  setTimeout(function(){
+    $('#dr img').show(3000);
+  },35000);
+
+
+  setTimeout(function(){
+    $('#dr').hide(2000);
+  },40000);
+
+
+  setTimeout(function(){
+    $('.wz3').show(3000);
+  },45000),
+  setTimeout(function(){
+    $('#ds img').show(3000);
+  },50000);
+
+  setTimeout(function(){
+    $('#ds').hide(2000);
+  },55000);
+
+
+  setTimeout(function(){
+    $('.wz4').show(3000);
+  },60000),
+  setTimeout(function(){
+    $('#dh img').show(3000);
+  },65000);
+  setTimeout(function(){
+    $('#dh').hide(2000);
+  },70000);
+
+
+  setTimeout(function(){
+    $('.zhwz').show(5000);
+  },68000),
+  setTimeout(function(){
+    $('.zhwz1').show(3000);
+  },73000),
+  setTimeout(function(){
+    $('.zhwz2').show(3000);
+  },78000),
+  setTimeout(function(){
+    $('.zhwz3').show(3000);
+  },83000);
+  setTimeout(function(){
+    $('.zhwz').hide(2000);
+  },9999000);
+})
