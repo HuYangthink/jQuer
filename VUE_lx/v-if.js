2cd8929ded19b5ff79d8aab12b835c4216031306
HuYangthink <41947204+HuYangthink@users.noreply.github.com>
2018-12-21 17:24:57 +0800

    Add files via upload

---
+++ VUE_lx/v-if.js
@@ -0,0 +1,32 @@
+new Vue({
+  el:"#vue-app",
+  data:{
+    error:false,
+    success:false
+  },
+  methods:{
+
+  },
+  components:{
+
+  }
+});
+
+new Vue({
+  el:'#vuw-app',
+  data:{
+    characters:['Mario',"Luffy","Yoshi"],
+    users:[
+      {name:'Henty',age:30},
+      {name:'Luffy',age:35},
+      {name:'Yoshi',age:24}
+
+    ]
+  },
+  methods: {
+
+  },
+  components:{
+
+  }
+})
